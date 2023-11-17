aef379fcee2eebf1a445f322fc20b468c38284fe
abdur-rafi <abdurrafi403@gmail.com>
2023-11-17 21:19:56 +0600

    best results brought in

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 # cd $1
 # list current dir and run a for loop
 
+filePrefix="ewqfm-%sat-c-f-n-f-v2"
 for file in $(ls $1)
 # for file in "1X-500-500"
 do
@@ -14,28 +15,28 @@ do
         # for loop for files inside the $file directory
         # > $1/$file/wqfm-v1.2-scores.txt
         # > $1/$file/ewqfm-cons-flat-norm-flat-score.txt
-        > $1/$file/ewqfm-cons-flat-norm-flat-score.txt
+        > $1/$file/$filePrefix-score.txt
 
 
         for file2 in $(ls $1/$file)
         do
-            echo $file2
             # check if file is a directory
             if [ -d $1/$file/$file2 ]
             then
+                echo $file2
                 # if directory then run the script recursively
                 # echo $file2
                 ./raxml-ng --redo --consense MRE --tree $1/$file/$file2/all_gt.tre --prefix $1/$file/$file2/cons >> ./raxml-ng.log
                 consOut=$1/$file/$file2/cons.raxml.consensusTreeMRE
                 python consensusCleaner.py < $consOut > $1/$file/$file2/cons.tre
                 python treeCleaner.py < $1/$file/$file2/all_gt.tre > $1/$file/$file2/all_gt_cleaned.tre 
-                /usr/bin/env /usr/lib/jvm/java-17-openjdk-amd64/bin/java  -XX:+ShowCodeDetailsInExceptionMessages -cp /home/abdur-rafi/.config/Code/User/workspaceStorage/da91ba3e148e5727246c82da7f9911d2/redhat.java/jdt_ws/E-WQFM_731a4073/bin src.Main $1/$file/$file2/all_gt_cleaned.tre $1/$file/$file2/cons.tre $1/$file/$file2/ewqfm-out-cons-flat-norm-flat.txt
-                python ./rfScoreCalculator/getFpFn.py -t $1/true_tree_trimmed -e $1/$file/$file2/ewqfm-out-cons-flat-norm-flat.txt >> $1/$file/ewqfm-cons-flat-norm-flat-score.txt
+                /usr/bin/env /usr/lib/jvm/java-17-openjdk-amd64/bin/java  -XX:+ShowCodeDetailsInExceptionMessages -cp /home/abdur-rafi/.config/Code/User/workspaceStorage/da91ba3e148e5727246c82da7f9911d2/redhat.java/jdt_ws/E-WQFM_731a4073/bin src.Main $1/$file/$file2/all_gt_cleaned.tre $1/$file/$file2/cons.tre $1/$file/$file2/$filePrefix-tree.txt
+                python ./rfScoreCalculator/getFpFn.py -t $1/true_tree_trimmed -e $1/$file/$file2/$filePrefix-tree.txt >> $1/$file/$filePrefix-score.txt
                 # python ./rfScoreCalculator/getFpFn.py -t $1/true_tree_trimmed -e $1/$file/$file2/wQFM-v1.2-best.tre >> $1/$file/wqfm-v1.2-scores.txt
             fi
         done
         # python ./rfAverager.py < $1/$file/wqfm-v1.2-scores.txt > $1/$file/wqfm-v1.2-scores-avg.txt
-        python ./rfAverager.py < $1/$file/ewqfm-cons-flat-norm-flat-score.txt > $1/$file/avg-ewqfm-cons-flat-norm-flat-score.txt
+        python ./rfAverager.py < $1/$file/$filePrefix-score.txt > $1/$file/avg-$filePrefix-score.txt
         echo "$file done"
         # break
     fi
