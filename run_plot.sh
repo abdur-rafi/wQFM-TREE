d3945427c3bf682691d3c801f6958dfb0a7d85be
abdur-rafi <abdurrafi403@gmail.com>
2023-11-18 23:45:24 +0600

    should have been on percentage_satisfied branch

--- run_plot.sh
+++ run_plot.sh
@@ -1,9 +1,9 @@
 bash run.sh ../run/15-taxon
-bash plotScore.sh ../run/15-taxon 15_svr_singleton_c_f_n_f_v2_vcfnf
+bash plotScore.sh ../run/15-taxon 15_svr_singleton_vs_wqfm_all
 
 bash run.sh ../run/37-taxon
-bash plotScore.sh ../run/37-taxon 37_svr_singleton_c_f_n_f_v2_vcfnf
+bash plotScore.sh ../run/37-taxon 37_svr_singleton_vs_wqfm_all
 
 
 bash run.sh ../run/48-taxon
-bash plotScore.sh ../run/48-taxon 48_svr_singleton_c_f_n_f_v2_vcfnf
+bash plotScore.sh ../run/48-taxon 48_svr_singleton_vs_wqfm_all
