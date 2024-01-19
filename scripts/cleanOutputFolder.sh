914248063534da43141cfda0a21fb8b45ac43ec1
abdur-rafi <abdurrafi403@gmail.com>
2024-01-19 11:33:23 +0600

    species trees added

--- scripts/cleanOutputFolder.sh
+++ scripts/cleanOutputFolder.sh
@@ -1,6 +1,7 @@
 # declare -a allowed=("astral2-200-tree.txt" "astral2-50-tree.txt" "ewqfm-50-resolved-tree.txt" "ewqfm-200-resolved-tree.txt" "ewqfm-50-tree.txt" "ewqfm-200-tree.txt" "treeqmc-50-tree.txt" "treeqmc-200-tree.txt" "ewqfm-paup-nq-A-tree.txt"  "ewqfm-with-resolved-gene-trees-tree.txt" "treeqmc-tree.txt" )
 # declare -a allowed=("ewqfm-paup-nq-A-tree.txt")
-declare -a allowed=("astral-tree.txt")
+# declare -a allowed=("astral-tree.txt")
+declare -a allowed=("sp-cleaned")
 root=$1
 for folder in $(ls $root); do
     echo $folder
