d13ed43ab2856b9b8251bbf61466500003c672f5
abdur-rafi <abdurrafi403@gmail.com>
2024-01-27 20:22:48 +0600

    jar created

--- scripts/averager.sh
+++
@@ -1,9 +0,0 @@
-scorePath=results/Astral_2_datasets/scores-by-model-cond
-
-# for modelCond in $(ls $scorePath); do
-for modelCond in model.1000.2000000.0.000001; do
-    for file in $(ls $scorePath/$modelCond); do
-        echo $modelCond $file
-        python ./scripts/rfAverager.py < $scorePath/$modelCond/$file > $scorePath/$modelCond/avg-$file
-    done
-done
